074caa0b698b9d8a81bb087311835aa81d77a3ad
iieunji <iieunji023@gmail.com>
2024-11-08 13:52:06 +0900

    refactor: 컴포넌트 스캔과 의존관계 자동 주입 - @ComponentScan - @Component - @Autowired - @Bean을 통해 설정정보에 직접 등록X -> 컴포넌트 스캔을 통해 자동으로 스프링 빈 등록 - 스프링 빈을 자동 등록하기 때문에 의존 관계 주입 코드X -> @Autowired를 통해 의존관계 자동 주입

---
+++ core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+// 설정 정보이므로 @Configuration
+@Configuration
+// 스프링 빈을 긁어서 자동으로 스프링 빈을 끌어올리기 위함
+// Configuration 어노테이션이 붙은 애들을 다 빼겠다
+// -> AppConfig도 Configuration 어노테이션 붙어있는데 이럴 경우 충돌이 날 수 있음
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
