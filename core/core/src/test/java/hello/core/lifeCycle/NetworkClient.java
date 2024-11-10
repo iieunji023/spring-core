79041ab0443846c8f7302d3e82ec3abed3faf924
iieunji023 <iieunji023@gmail.com>
2024-11-10 12:46:25 +0900

    Test: 빈 생명주기 콜백 지원 - 인터페이스 - InitializingBean, DisposableBean - 초기화, 소멸 인터페이스의 단점

--- core/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifeCycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
 
     }
 
@@ -27,5 +29,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close= " + url);
+    }
+
+    // 의존 관계 주입이 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
