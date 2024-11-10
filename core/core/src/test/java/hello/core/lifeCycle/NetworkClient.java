ce299dfcf082d00be330e0a8dfa8b68a612478cd
iieunji023 <iieunji023@gmail.com>
2024-11-10 13:09:06 +0900

    Test: 빈 생명주기 콜백 지원 - 빈 등록 초기화, 소멸 메서드 지정

--- core/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -3,13 +3,11 @@ package hello.core.lifeCycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-
-
     }
 
     public void setUrl(String url) {
@@ -19,7 +17,6 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     // 서비스 시작시 호출
     public void connect(){
         System.out.println("connect = " + url);
-
     }
 
     public void call(String message){
@@ -32,17 +29,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 의존 관계 주입이 끝나면 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
     // 빈이 종료될 때 호출
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
